32f40f104df1f0602a391fc857c77ef62a158a8d
Nuriddin-775 <alijonovnuriddin271@gmail.com>
2023-04-19 14:02:34 +0500

    New key added

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,6 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE =  10;
-export const KEY = 'e850b3a4-bb7b-43ff-b765-e8a54096df20';
+// export const KEY = 'e850b3a4-bb7b-43ff-b765-e8a54096df20';
+export const KEY = 'f8615194-1040-4871-b017-c494a4a4c4ad';
 export const MODAL_CLOSE_SEC = 2.5;
